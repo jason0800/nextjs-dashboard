4c8a1baf19f6754fe751123d8835ac07e9d4c365
Jason Wang <jasonwang332@gmail.com>
2025-08-13 15:27:23 +1000

    added loading.tsx to dashboard

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,7 +17,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 6000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
@@ -67,7 +67,7 @@ export async function fetchCardData() {
       customerCountPromise,
       invoiceStatusPromise,
     ]);
-    
+
     const numberOfInvoices = Number(data[0][0].count ?? '0');
     const numberOfCustomers = Number(data[1][0].count ?? '0');
     const totalPaidInvoices = formatCurrency(data[2][0].paid ?? '0');
