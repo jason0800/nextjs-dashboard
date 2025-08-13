33bbbeeeef3e475991528cc59f44b5b9275a74a3
Jason Wang <jasonwang332@gmail.com>
2025-08-13 16:58:34 +1000

    wrapped latest invoice in suspense

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -31,6 +31,11 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
